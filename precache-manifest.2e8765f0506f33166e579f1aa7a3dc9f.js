4f69fe81fcd635759f1289dcd3550a8d6b29d2a2
Andrew Ilin <ilin.serv@gmail.com>
2019-02-13 17:23:26 +0300

    Updates

---
+++ precache-manifest.2e8765f0506f33166e579f1aa7a3dc9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "422aba8639312ac889d34200226a5c4e",
+    "url": "/exchange-rates-app/static/media/icons-16.422aba86.woff"
+  },
+  {
+    "revision": "120450a5bd18838638c9",
+    "url": "/exchange-rates-app/static/css/main.f081429f.chunk.css"
+  },
+  {
+    "revision": "778a0a66de37e44766e3",
+    "url": "/exchange-rates-app/static/js/runtime~main.778a0a66.js"
+  },
+  {
+    "revision": "5502b03b7b07c388f20f77fa469a1f47",
+    "url": "/exchange-rates-app/static/media/icons-20.5502b03b.ttf"
+  },
+  {
+    "revision": "e38818a11fcef18b1d85",
+    "url": "/exchange-rates-app/static/js/2.e38818a1.chunk.js"
+  },
+  {
+    "revision": "7f037192fcdef96884010177e90e8c5f",
+    "url": "/exchange-rates-app/static/media/icons-16.7f037192.eot"
+  },
+  {
+    "revision": "120450a5bd18838638c9",
+    "url": "/exchange-rates-app/static/js/main.120450a5.chunk.js"
+  },
+  {
+    "revision": "a82b41082b04f774d82317dff6874d14",
+    "url": "/exchange-rates-app/static/media/icons-16.a82b4108.ttf"
+  },
+  {
+    "revision": "f1bd87971a332ecb21b4ac1d19537fa3",
+    "url": "/exchange-rates-app/static/media/icons-20.f1bd8797.eot"
+  },
+  {
+    "revision": "52f2f35bf3a635ca696e95cc33cf12fa",
+    "url": "/exchange-rates-app/static/media/icons-20.52f2f35b.woff"
+  },
+  {
+    "revision": "e38818a11fcef18b1d85",
+    "url": "/exchange-rates-app/static/css/2.9f8779db.chunk.css"
+  },
+  {
+    "revision": "ccb199ce6becc88b1bd3b9b128fb532e",
+    "url": "/exchange-rates-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/exchange-rates-app/index.html","51ae635f5be4c0150b8ba13575dfd629"],["/exchange-rates-app/static/css/main.75c69a56.css","f0b92bf8196f4f499304e26a61fc766b"],["/exchange-rates-app/static/js/main.5a2ba934.js","4b5314dd58af29bfd22539ccf87128d8"],["/exchange-rates-app/static/media/icons-16.76957eb0.ttf","76957eb0b3f1dd52766872b7dd6c04d6"],["/exchange-rates-app/static/media/icons-16.c22f3806.eot","c22f3806638273bd9e764fea0a812c51"],["/exchange-rates-app/static/media/icons-16.d13388be.woff","d13388bee1190a551bb5d9c9f953a779"],["/exchange-rates-app/static/media/icons-20.062208ea.eot","062208ea882dd2a7e2fa8552c6f32e3b"],["/exchange-rates-app/static/media/icons-20.6d716ba4.ttf","6d716ba49663310796d6cf4d6d8b65b2"],["/exchange-rates-app/static/media/icons-20.f56d7d34.woff","f56d7d3470cf9d60593812561aff63c4"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,a){var r=new URL(e);return a&&r.pathname.match(a)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],a=new URL(t,self.location),r=createCacheKey(a,hashParamName,n,/\.\w{8}\./);return[a.toString(),r]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(a){return setOfCachedUrls(a).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return a.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),a="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,a),e=urlsToCacheKeys.has(n));var r="/exchange-rates-app/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(r,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/exchange-rates-app/precache-manifest.2e8765f0506f33166e579f1aa7a3dc9f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/exchange-rates-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
